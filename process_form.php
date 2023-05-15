a8fc1009f8cf9317ed635ad9a31c671ba80c3cd5
LarryIra <86225301+LarryIra@users.noreply.github.com>
2023-05-15 21:22:40 +0200

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,41 +1,30 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Get the form fields and sanitize them
-  $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-  $start_datetime = filter_var($_POST["start-datetime"], FILTER_SANITIZE_STRING);
-  $end_datetime = filter_var($_POST["end-datetime"], FILTER_SANITIZE_STRING);
-  $budget = filter_var($_POST["budget"], FILTER_SANITIZE_STRING);
-  $destination = filter_var($_POST["destination"], FILTER_SANITIZE_STRING);
-  $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+    $to = "larrywayne309@gmail.com";
+    $subject = "Booking Request";
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $startDateTime = $_POST["start-datetime"];
+    $endDateTime = $_POST["end-datetime"];
+    $budget = $_POST["budget"];
+    $destination = $_POST["destination"];
+    $message = $_POST["message"];
 
-  // Set the recipient email address
-  $to = "larrywayne309@gmail.com";
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Start Date & Time: $startDateTime\n";
+    $body .= "End Date & Time: $endDateTime\n";
+    $body .= "Budget: $budget\n";
+    $body .= "Destination: $destination\n";
+    $body .= "Message: $message\n";
 
-  // Set the email subject
-  $subject = "New Booking Request";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
 
-  // Set the email message
-  $message = "Name: $name\n\n"
-           . "Email: $email\n\n"
-           . "Start Date & Time: $start_datetime\n\n"
-           . "End Date & Time: $end_datetime\n\n"
-           . "Budget: $budget\n\n"
-           . "Destination: $destination\n\n"
-           . "Message:\n$message";
-
-  // Set the email headers
-  $headers = "From: $name <$email>\r\n";
-  $headers .= "Reply-To: $email\r\n";
-  $headers .= "X-Mailer: PHP/" . phpversion();
-
-  // Send the email
-  if (mail($to, $subject, $message, $headers)) {
-    echo "<script>alert('Your message has been sent successfully!');</script>";
-    echo "<meta http-equiv='refresh' content='0'>";
-  } else {
-    echo "<script>alert('There was an error sending your message. Please try again later.');</script>";
-    echo "<meta http-equiv='refresh' content='0'>";
-  }
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for your booking request. We will contact you shortly.";
+    } else {
+        echo "Sorry, an error occurred while processing your request. Please try again later.";
+    }
 }
 ?>
